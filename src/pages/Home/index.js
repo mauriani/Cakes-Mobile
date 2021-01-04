5757283972f02a0772c5fb60175e1796dceed730
mauriani <maurianim@gmail.com>
2021-01-04 17:01:13 -0300

    atualizando layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -65,6 +65,105 @@ export default class Home extends Component {
             </ButtonAdd>
           </View>
         </CardProduct>
+
+        <CardProduct>
+          <Image
+            source={{
+              uri:
+                'https://images.unsplash.com/photo-1535141192574-5d4897c12636?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=335&q=80',
+            }}
+            resizeMode="cover"
+          />
+
+          <Details>
+            <TitleProduct>
+              Bolo de Chocolate com Recheio de Brigadeiro
+            </TitleProduct>
+
+            <TitlePrice>R$ 100,00</TitlePrice>
+          </Details>
+
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Details>
+              <Icon name="star" size={25} color="#ff9e00" />
+              <Icon name="star" size={25} color="#ff9e00" />
+              <Icon name="star" size={25} color="#ff9e00" />
+              <Icon name="star" size={25} color="#ff9e00" />
+              <Icon name="star" size={25} color="#ff9e00" />
+            </Details>
+
+            <ButtonAdd>
+              <ButtonAddText>ADD</ButtonAddText>
+              <Icon name="add-circle-outline" size={28} color="#fff" />
+            </ButtonAdd>
+          </View>
+        </CardProduct>
+
+        <CardProduct>
+          <Image
+            source={{
+              uri:
+                'https://images.unsplash.com/photo-1535141192574-5d4897c12636?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=335&q=80',
+            }}
+            resizeMode="cover"
+          />
+
+          <Details>
+            <TitleProduct>
+              Bolo de Chocolate com Recheio de Brigadeiro
+            </TitleProduct>
+
+            <TitlePrice>R$ 100,00</TitlePrice>
+          </Details>
+
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Details>
+              <Icon name="star" size={25} color="#ff9e00" />
+              <Icon name="star" size={25} color="#ff9e00" />
+              <Icon name="star" size={25} color="#ff9e00" />
+              <Icon name="star" size={25} color="#ff9e00" />
+              <Icon name="star" size={25} color="#ff9e00" />
+            </Details>
+
+            <ButtonAdd>
+              <ButtonAddText>ADD</ButtonAddText>
+              <Icon name="add-circle-outline" size={28} color="#fff" />
+            </ButtonAdd>
+          </View>
+        </CardProduct>
+
+        <CardProduct>
+          <Image
+            source={{
+              uri:
+                'https://images.unsplash.com/photo-1535141192574-5d4897c12636?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=335&q=80',
+            }}
+            resizeMode="cover"
+          />
+
+          <Details>
+            <TitleProduct>
+              Bolo de Chocolate com Recheio de Brigadeiro
+            </TitleProduct>
+
+            <TitlePrice>R$ 100,00</TitlePrice>
+          </Details>
+
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Details>
+              <Icon name="star" size={25} color="#ff9e00" />
+              <Icon name="star" size={25} color="#ff9e00" />
+              <Icon name="star" size={25} color="#ff9e00" />
+              <Icon name="star" size={25} color="#ff9e00" />
+              <Icon name="star" size={25} color="#ff9e00" />
+            </Details>
+
+            <ButtonAdd>
+              <ButtonAddText>ADD</ButtonAddText>
+              <Icon name="add-circle-outline" size={28} color="#fff" />
+            </ButtonAdd>
+          </View>
+        </CardProduct>
       </Container>
     );
   }

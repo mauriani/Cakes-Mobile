8999d1617c199fcbc6fbbc7c1c210ea6c7c22ecb
mauriani <maurianim@gmail.com>
2021-01-06 16:31:09 -0300

    testes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {View, ScrollView} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
@@ -22,7 +22,7 @@ export default class Home extends Component {
 
   async componentDidMount() {
     try {
-      const response = await api.get('/products');
+      const response = await api.get('products');
       this.setState({products: response.data});
     } catch (err) {
       console.error(err);
@@ -30,140 +30,39 @@ export default class Home extends Component {
   }
 
   render() {
-    console.log(this.state.products);
+    const {products} = this.state;
+    console.log('oi', products);
     return (
       <Container>
-        <CardProduct>
-          <Image
-            source={{
-              uri:
-                'https://images.unsplash.com/photo-1535141192574-5d4897c12636?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=335&q=80',
-            }}
-            resizeMode="cover"
-          />
+        <ScrollView>
+          {products.map((product) => (
+            <CardProduct key={String(product.id)}>
+              <Image source={product.image} resizeMode="contain" />
 
-          <Details>
-            <TitleProduct>
-              Bolo de Chocolate com Recheio de Brigadeiro
-            </TitleProduct>
+              <Details>
+                <TitleProduct>{product.title}</TitleProduct>
 
-            <TitlePrice>R$ 100,00</TitlePrice>
-          </Details>
+                <TitlePrice>{product.price}</TitlePrice>
+              </Details>
 
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <Details>
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-            </Details>
+              <View
+                style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Details>
+                  <Icon name="star" size={25} color="#ff9e00" />
+                  <Icon name="star" size={25} color="#ff9e00" />
+                  <Icon name="star" size={25} color="#ff9e00" />
+                  <Icon name="star" size={25} color="#ff9e00" />
+                  <Icon name="star" size={25} color="#ff9e00" />
+                </Details>
 
-            <ButtonAdd>
-              <ButtonAddText>ADD</ButtonAddText>
-              <Icon name="add-circle-outline" size={28} color="#fff" />
-            </ButtonAdd>
-          </View>
-        </CardProduct>
-
-        <CardProduct>
-          <Image
-            source={{
-              uri:
-                'https://images.unsplash.com/photo-1535141192574-5d4897c12636?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=335&q=80',
-            }}
-            resizeMode="cover"
-          />
-
-          <Details>
-            <TitleProduct>
-              Bolo de Chocolate com Recheio de Brigadeiro
-            </TitleProduct>
-
-            <TitlePrice>R$ 100,00</TitlePrice>
-          </Details>
-
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <Details>
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-            </Details>
-
-            <ButtonAdd>
-              <ButtonAddText>ADD</ButtonAddText>
-              <Icon name="add-circle-outline" size={28} color="#fff" />
-            </ButtonAdd>
-          </View>
-        </CardProduct>
-
-        <CardProduct>
-          <Image
-            source={{
-              uri:
-                'https://images.unsplash.com/photo-1535141192574-5d4897c12636?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=335&q=80',
-            }}
-            resizeMode="cover"
-          />
-
-          <Details>
-            <TitleProduct>
-              Bolo de Chocolate com Recheio de Brigadeiro
-            </TitleProduct>
-
-            <TitlePrice>R$ 100,00</TitlePrice>
-          </Details>
-
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <Details>
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-            </Details>
-
-            <ButtonAdd>
-              <ButtonAddText>ADD</ButtonAddText>
-              <Icon name="add-circle-outline" size={28} color="#fff" />
-            </ButtonAdd>
-          </View>
-        </CardProduct>
-
-        <CardProduct>
-          <Image
-            source={{
-              uri:
-                'https://images.unsplash.com/photo-1535141192574-5d4897c12636?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=335&q=80',
-            }}
-            resizeMode="cover"
-          />
-
-          <Details>
-            <TitleProduct>
-              Bolo de Chocolate com Recheio de Brigadeiro
-            </TitleProduct>
-
-            <TitlePrice>R$ 100,00</TitlePrice>
-          </Details>
-
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <Details>
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-              <Icon name="star" size={25} color="#ff9e00" />
-            </Details>
-
-            <ButtonAdd>
-              <ButtonAddText>ADD</ButtonAddText>
-              <Icon name="add-circle-outline" size={28} color="#fff" />
-            </ButtonAdd>
-          </View>
-        </CardProduct>
+                <ButtonAdd>
+                  <ButtonAddText>ADD</ButtonAddText>
+                  <Icon name="add-circle-outline" size={28} color="#fff" />
+                </ButtonAdd>
+              </View>
+            </CardProduct>
+          ))}
+        </ScrollView>
       </Container>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.148:3131',
+  baseURL: 'http://192.168.1.148:3000',
 });
 
 export default api;
